87e98be207f07de5a120b0bbffceb23e6e1aa862
pranaykumar <pranay.mishra@gmail.com>
2014-08-27 23:33:40 +0530

    1. Added partial view for ADD new provider 2. updated app.js to handle add new provider feature and get all profiles for a given provider 3. style changes

--- WebContent/src/js/app.js
+++ WebContent/src/js/app.js
@@ -1,10 +1,13 @@
 var myApp = angular.module('myApp', []);
-var IP = '192.168.2.7';
+var IP = 'restip';
 
 myApp.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/providers', {
 		templateUrl : 'partialview/providers.html',
 		controller : 'providersCntrl'
+	}).when('/providers/addprovider', {
+		templateUrl : 'partialview/AddProvider.html',
+		controller : 'AddProviderCntrl'
 	}).when('/providers/:provider_id', {
 		templateUrl : 'partialview/provider_detail.html',
 		controller : 'providerDtlCntrl'
@@ -14,19 +17,45 @@ myApp.config([ '$routeProvider', function($routeProvider) {
 } ]);
 
 myApp.controller('providersCntrl', function($scope, $http) {
-	$http.get('http://'+IP+':3000/api/providers').success(
-			function(data) {
-				$scope.Providers = data;
-			});
+	$http.get('http://' + IP + ':3000/api/providers').success(function(data) {
+		$scope.Providers = data;
+	});
 });
 
 myApp.controller('providerDtlCntrl', function($scope, $http, $routeParams) {
 	$http.get(
-			'http://'+IP+':3000/api/providers/'
-					+ $routeParams.provider_id).success(function(data) {
-		console.log(data);
-		$scope.provider_id = data[0].provider_id;
-		$scope.name = data[0].name;
-		$scope.email = data[0].email;
+			'http://' + IP + ':3000/api/providers/' + $routeParams.provider_id)
+			.success(function(data) {
+				console.log(data);
+				$scope.provider_id = data[0].provider_id;
+				$scope.name = data[0].name;
+				$scope.email = data[0].email;
+			});
+	
+	$http.get(
+			'http://' + IP + ':3000/api/providers/' + $routeParams.provider_id
+					+ '/profiles').success(function(data) {
+		$scope.Profiles = data;
 	});
+});
+
+myApp.controller('AddProviderCntrl', function($scope, $http) {
+	$scope.name = 'Enter Provider Name';
+	$scope.email = 'Enter Vaild Email';
+
+	$scope.insertData = function() {
+		$http({
+			method : 'POST',
+			url : 'http://'+IP+':3000/api/providers',
+			data : {
+				"name" : $scope.name,
+				"email" : $scope.email,
+			},
+			headers : {
+				'content-type' : 'application/json'
+			}
+		}).success(function(data) {
+			console.log(data);
+		});
+	};
 });
